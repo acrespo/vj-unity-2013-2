c9a44a118e6d01e6b072ac4da791fec7abc00272
Juan Pablo Civile <elementohb@gmail.com>
2013-08-20 19:14:30 -0300

    Initial commit

---
+++ Assets/Alien Prefab/AlienMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienMovement : MonoBehaviour {
+	
+	private CharacterController controller;
+	
+	private SpriteAnimation animator;
+
+	public float movementBeforeTurn = 30;
+	
+	public float delta = 1;
+	
+	private float movementInDirection = 0;
+	
+	private float direction = 1;
+	
+	private int updatesElapsed = 0;
+	
+	public int updatesTillMove = 3;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		animator = GetComponent<SpriteAnimation>();
+		
+		// Start in the center of the movement area
+		movementInDirection = movementBeforeTurn / 2;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		if (updatesElapsed < updatesTillMove) {
+			updatesElapsed++;
+			return;
+		} else {
+			updatesElapsed = 0;
+		}
+		
+		animator.Animate();
+		movementInDirection += delta;
+		
+		Vector3 move = new Vector3(delta * direction, 0, 0);
+		
+		if (movementInDirection >= movementBeforeTurn) {
+			direction = -direction;
+			movementInDirection = 0;
+			
+			move.y = -3;
+		}
+		
+		controller.Move(move); 
+	}
+	
+}
---
+++ Assets/Alien Prefab/SpriteAnimation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnimation : MonoBehaviour {
+	
+	private MeshFilter filter;
+	
+	public int frames;
+	
+	public float frameWidth;
+	
+	private int currentFrame = -1;
+	
+	// Use this for initialization
+	void Start () {
+		filter = GetComponent<MeshFilter>();
+		renderer.sharedMaterial.SetTextureScale("_MainTex", new Vector2(0.5f, 1.0f));
+		Animate();
+	}
+	
+	public void Animate () {
+		
+		currentFrame++;
+		if (currentFrame >= frames) {
+			currentFrame = 0;
+			
+		}
+
+		Vector2 offset = new Vector2((float)currentFrame / (float)frames, 0);
+		renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
