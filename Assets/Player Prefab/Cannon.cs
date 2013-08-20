d9b09637ab20627e90e60c002f504674949d80b2
Juan Pablo Civile <elementohb@gmail.com>
2013-08-20 20:38:13 -0300

    Simple Cannon & Laser (with no collission and crappy movement)

---
+++ Assets/Player Prefab/Cannon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+	
+	public GameObject laserPrefab;
+	
+	private GameObject laser;
+
+	// Use this for initialization
+	void Start () {
+		laser = (GameObject) GameObject.Instantiate(laserPrefab);
+		laser.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown(KeyCode.Space) && !laser.activeSelf) {
+            laser.SetActive(true);
+			
+			laser.transform.position = transform.position;
+			laser.rigidbody.AddForce(Vector3.up * 1000);
+        }
+	}
+}
