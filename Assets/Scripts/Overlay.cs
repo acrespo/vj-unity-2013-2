56bbf85b0a8149ccb8cac2e36d280478033e4faa
Juan Pablo Civile <elementohb@gmail.com>
2013-08-20 23:16:24 -0300

    Modify the way score was shown to be more MVCy

---
+++ Assets/Scripts/Overlay.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Overlay : MonoBehaviour {
+	
+	public GUISkin skin;
+	
+	void OnGUI() {
+		GUI.Label(new Rect(10, 0, 100, 20), "Score:  " + ScoreKeeper.Instance.Score.ToString(), skin.label);
+	}
+}
--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class ScoreKeeper : Singleton<ScoreKeeper> {
 	
 	private int score = 0;
-
-	void OnGUI() {
-		GUI.Label(new Rect(10, 0, 100, 20), score.ToString());
-	}
 	
+	public int Score {
+		get {
+			return score;
+		}
+	}
+
 	public void addScore(int score) {
 		this.score += score;
 	}
