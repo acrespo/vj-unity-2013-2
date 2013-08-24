462f8102e521f5e1b73c5e4b165e89fa26ea4ae9
acrespo <alvaro.andres.crespo@gmail.com>
2013-08-24 17:21:59 -0300

    Bunkers are now destroyable

---
+++ Assets/Objects/Bunker/BunkerShootable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteAnimation))]
+public class BunkerShootable : Shootable {
+	
+	public int lives;
+	
+	private SpriteAnimation animator;
+
+	void Start () {
+		animator = GetComponent<SpriteAnimation>();
+		
+	}
+	
+	public override void Hit () {
+		if (lives > 1) {
+			animator.Animate();
+			lives--;
+		} else {
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
