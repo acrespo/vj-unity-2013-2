98707ad24fff82563df0183c5c981f130d6211a9
acrespo <alvaro.andres.crespo@gmail.com>
2013-08-24 17:34:49 -0300

    Fixed bunkers positions

--- Assets/Scripts/BunkerCreator.cs
+++ Assets/Scripts/BunkerCreator.cs
@@ -17,7 +17,7 @@ public class BunkerCreator : MonoBehaviour {
 	void createBunkerLine(Texture bunkerTexture, float y) {
 		
 		for (int i = 0; i < 4; i++) {	
-			GameObject bunker = (GameObject) GameObject.Instantiate(bunkerPrefab, new Vector3(distanceBetweenBunkers * (i - 1.5f), y, 300), Quaternion.identity);
+			GameObject bunker = (GameObject) GameObject.Instantiate(bunkerPrefab, new Vector3(distanceBetweenBunkers * (i - 2) + distanceBetweenBunkers/2, y, 300), Quaternion.identity);
 			bunker.renderer.material.mainTexture = bunkerTexture;			
 		}
 	}
