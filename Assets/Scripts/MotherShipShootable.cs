6dbe36891a7c3b7b756a18572ec051b69c541fdc
acrespo <alvaro.andres.crespo@gmail.com>
2013-08-27 14:37:24 -0300

    Mothership doesnt die forever. Or another spawns if u will.

---
+++ Assets/Scripts/MotherShipShootable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotherShipShootable : Shootable {
+
+	public int score;
+
+	public override void Hit () {
+		renderer.enabled = false;
+		transform.position = new Vector3(-110, 60, 400);
+		ScoreKeeper.Instance.alienDied(score);
+		SoundManager.Instance.Play(SoundManager.Instance.alienExplosion);
+	}
+}
