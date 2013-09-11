f024dba8c3c700fda427be585968518c9089d7a4
Juan Pablo Civile <elementohb@gmail.com>
2013-09-11 00:47:59 -0300

    Add unkillable blocks around the level

--- tp2/src/Assets/Scripts/World.cs
+++ tp2/src/Assets/Scripts/World.cs
@@ -5,6 +5,8 @@ public class World : MonoBehaviour {
 	
 	public GameObject mapBlock;
 	
+	public GameObject unkillable;
+	
 	public GameObject tank;
 	
 	private bool paused = false;
@@ -20,13 +22,20 @@ public class World : MonoBehaviour {
 		
 		string text = firstLevel.text;
 		
-		int i = 0, j = 0;
+		int i = 0, j = 0, width = 0;
 		foreach (char c in text) {
 			if (c == '\n') {
+				if (width == 0) {
+					width = i;
+				}
+				addBlock(unkillable, -1, j);
+				addBlock(unkillable, width, j);
+				
 				j++;
 				i = -1;
+				
 			} else if (c == '#') {
-				addBlock(i, j);
+				addBlock(mapBlock, i, j);
 			} else if (c == 'P') {
 				GameObject tankObj = Instantiate(tank) as GameObject;
 				tankObj.transform.position = new Vector3(i, 0.51f, j);
@@ -36,6 +45,13 @@ public class World : MonoBehaviour {
 			i++;
 		}
 		
+		addBlock(unkillable, -1, j);
+		addBlock(unkillable, width, j);
+		
+		for (i = -1; i < width + 1; i++) {
+			addBlock(unkillable, i, -1);
+			addBlock(unkillable, i, j);
+		}
 	}
 	
 	void Update () {
@@ -52,8 +68,8 @@ public class World : MonoBehaviour {
 		}
 	}
 	
-	private void addBlock(int x, int z) {
-		GameObject block = Instantiate(mapBlock) as GameObject;
+	private void addBlock(GameObject type, int x, int z) {
+		GameObject block = Instantiate(type) as GameObject;
 		block.transform.position = new Vector3(x, 0.51f, z);
 		block.transform.parent = transform;
 	}
