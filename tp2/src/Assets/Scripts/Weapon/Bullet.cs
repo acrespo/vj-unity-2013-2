9061dac9fcd9eedd1b4a42e460e93da84ce3aa71
Juan Pablo Civile <elementohb@gmail.com>
2013-09-11 00:23:38 -0300

    Make bullets die after a while

--- tp2/src/Assets/Scripts/Weapon/Bullet.cs
+++ tp2/src/Assets/Scripts/Weapon/Bullet.cs
@@ -5,10 +5,22 @@ public class Bullet : MonoBehaviour {
 	
 	public Team team;
 	
+	private float birth;
+	
+	void Start() {
+		birth = Time.time;
+	}
+	
 	void OnTriggerEnter(Collider collider) {
 		Shootable imp = collider.gameObject.GetComponent<Shootable>();
 		if (imp && imp.enabled && imp.team != team) {
 			imp.Hit();
+			GameObject.Destroy(gameObject);
+		}
+	}
+	
+	void FixedUpdate() {
+		if (Time.time - birth > 100) {
 			GameObject.Destroy(gameObject);
 		}
 	}
