851460f0739179cf3c083f344ce70273bae89038
Juan Pablo Civile <elementohb@gmail.com>
2013-09-11 00:23:47 -0300

    Add level loading from text files

--- tp2/src/Assets/Scripts/World.cs
+++ tp2/src/Assets/Scripts/World.cs
@@ -5,13 +5,36 @@ public class World : MonoBehaviour {
 	
 	public GameObject mapBlock;
 	
+	public GameObject tank;
+	
 	private bool paused = false;
+	
+	public TextAsset firstLevel;
 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 10; i++) {
-			addBlock(10, i);
+		
+		foreach (Transform child in transform) {
+			GameObject.Destroy(child.gameObject);
 		}
+		
+		string text = firstLevel.text;
+		
+		int i = 0, j = 0;
+		foreach (char c in text) {
+			if (c == '\n') {
+				j++;
+				i = -1;
+			} else if (c == '#') {
+				addBlock(i, j);
+			} else if (c == 'P') {
+				GameObject tankObj = Instantiate(tank) as GameObject;
+				tankObj.transform.position = new Vector3(i, 0.51f, j);
+			}
+			
+			i++;
+		}
+		
 	}
 	
 	void Update () {
@@ -29,7 +52,7 @@ public class World : MonoBehaviour {
 	}
 	
 	private void addBlock(int x, int z) {
-		GameObject block = (GameObject) GameObject.Instantiate(mapBlock);
+		GameObject block = Instantiate(mapBlock) as GameObject;
 		block.transform.position = new Vector3(x, 0.51f, z);
 	}
 }
