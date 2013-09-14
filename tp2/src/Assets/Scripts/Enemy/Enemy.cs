679e52f3359e066f217e68fe9a5de9adbee0bca5
Juan Pablo Civile <elementohb@gmail.com>
2013-09-14 19:22:56 -0300

    Enemys do a really basic random walk

--- tp2/src/Assets/Scripts/Enemy/Enemy.cs
+++ tp2/src/Assets/Scripts/Enemy/Enemy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 	
+	public float timeBetweenMoves = 1.0f;
+	
+	private float lastMoveTime = 0f;
+	
 	private Vector2 _spawnPoint;
 	
 	public Vector2 SpawnPoint {
@@ -13,6 +17,18 @@ public class Enemy : MonoBehaviour
 		
 		set {
 			_spawnPoint = value;
+		}
+	}
+	
+	void FixedUpdate() {
+		
+		if (Time.time > lastMoveTime + timeBetweenMoves) {
+			Vector3 direction = new Vector3(UnityEngine.Random.Range(-1, 2), 1, UnityEngine.Random.Range(-1, 2));
+			direction /= direction.magnitude * 2;
+			
+			GetComponent<CharacterMotor>().inputMoveDirection = direction;
+			
+			lastMoveTime = Time.time;
 		}
 	}
 }
