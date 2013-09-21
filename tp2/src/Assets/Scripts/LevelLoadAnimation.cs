4bb000ec2196b70c0241c12a6eef300fb672e374
Juan Pablo Civile <elementohb@gmail.com>
2013-09-21 15:12:25 -0300

    Remove useless logging

--- tp2/src/Assets/Scripts/LevelLoadAnimation.cs
+++ tp2/src/Assets/Scripts/LevelLoadAnimation.cs
@@ -23,19 +23,16 @@ public class LevelLoadAnimation
 		end = start + 3.0f;
 		camera = Camera.main.transform;
 		origin = camera.transform.position;
-		Debug.Log (cameraDestination);
 	}
 	
 	
 	public bool Update() {
 		
 		if (Time.realtimeSinceStartup >= end) {
-			Debug.Log ("Hello!");
 			return false;
 		}
 		
 		float delta = Time.realtimeSinceStartup - start;
-		Debug.Log (delta);
 		camera.position = Vector3.Slerp(origin, cameraDestination, delta / 3.0f);		
 		
 		return true;
