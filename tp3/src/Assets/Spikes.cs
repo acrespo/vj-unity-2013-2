ac8258939eb12b77b572b856652cb8c436d6d28c
Frederic <fred-2507@live.fr>
2013-10-26 19:57:40 -0300

    spikessss

---
+++ tp3/src/Assets/Spikes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spikes : MonoBehaviour
+{
+	public int damage = 2;
+	private GameObject player;
+	
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject == player) {
+			player.GetComponent<Player> ().Damage (damage);
+		}
+	}
+}
