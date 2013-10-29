53f4104585d9244977379b6f4ba4b95b24ce484c
acrespo <alvaro.andres.crespo@gmail.com>
2013-10-29 20:59:57 -0300

    Fixed RoomTrigger.

--- tp3/src/Assets/Scripts/RoomTrigger.cs
+++ tp3/src/Assets/Scripts/RoomTrigger.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 
 public class RoomTrigger : MonoBehaviour {
 	
+	private bool alreadyTriggered = false;
+		
 	void OnTriggerEnter(Collider c) {
 		
-		if (c.gameObject.GetComponent<Player>() != null) {
+		if (c.gameObject.GetComponent<Player>() != null && !alreadyTriggered) {
 			foreach (Transform t in transform.parent) {
 				if (t.gameObject.GetComponent<Enemy>()) {
+					alreadyTriggered = true;
 					t.gameObject.GetComponent<Enemy>().TriggerChase();
 				} else if (t != transform) {
 					t.gameObject.animation.Play("out-open-slowly");
